c5711d1c757333f5d5b3f40b462587e61daa639c
iqvverVIVO <iqvver@gmail.com>
2025-01-23 23:47:20 +0200

    Updates

--- _nuxt/CH1ua4os.js
+++
@@ -1,1 +0,0 @@
-import{u as t}from"./BorlRb2F.js";import{k as r,r as a}from"./BE6466Nr.js";const f=r({__name:"default",setup(o){return t({title:"Заводчанин З/П"}),(e,s)=>a(e.$slots,"default")}});export{f as default};
