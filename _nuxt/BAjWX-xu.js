3fe608f4ead49e4dbd714f093a755e443669ab1c
iqvverVIVO <iqvver@gmail.com>
2025-01-24 21:18:42 +0200

    Updates

---
+++ _nuxt/BAjWX-xu.js
@@ -0,0 +1,1 @@
+import{k as t,r}from"./D7FgQlN6.js";import{u as a}from"./B-1tynIz.js";const f=t({__name:"default",setup(o){return a({title:"Заводчанин З/П"}),(e,s)=>r(e.$slots,"default")}});export{f as default};
--- _nuxt/DIkSStAK.js
+++
@@ -1,1 +0,0 @@
-import{k as t,r}from"./B146R4IS.js";import{u as a}from"./wQ4i0pb_.js";const f=t({__name:"default",setup(o){return a({title:"Заводчанин З/П"}),(e,s)=>r(e.$slots,"default")}});export{f as default};
